dfeedbe53c2be2812af83e0dbe240b80cbcdd8c5
Atan0707 <harizhakim85@gmail.com>
2026-04-07 20:19:19 +0800

    Add other region route

--- fe/src/lib/region.ts
+++ fe/src/lib/region.ts
@@ -1,4 +1,12 @@
-export type ApiRegionPrefix = '' | '/kangar' | '/alor-setar'
+export type ApiRegionPrefix =
+  | ''
+  | '/kangar'
+  | '/alor-setar'
+  | '/kota-bharu'
+  | '/kuala-terengganu'
+  | '/ipoh'
+  | '/seremban-a'
+  | '/seremban-b'
 
 type RegionDefinition = {
   prefix: ApiRegionPrefix
@@ -25,6 +33,46 @@ const REGIONS: RegionDefinition[] = [
     latMax: 6.35,
     lonMin: 100.2,
     lonMax: 100.65,
+  },
+  {
+    prefix: '/kota-bharu',
+    name: 'Kota Bharu',
+    latMin: 4.8,
+    latMax: 6.3,
+    lonMin: 101.8,
+    lonMax: 102.65,
+  },
+  {
+    prefix: '/kuala-terengganu',
+    name: 'Kuala Terengganu',
+    latMin: 5.0,
+    latMax: 5.6,
+    lonMin: 102.9,
+    lonMax: 103.25,
+  },
+  {
+    prefix: '/ipoh',
+    name: 'Ipoh',
+    latMin: 3.85,
+    latMax: 4.9,
+    lonMin: 100.9,
+    lonMax: 101.35,
+  },
+  {
+    prefix: '/seremban-a',
+    name: 'Seremban',
+    latMin: 2.4,
+    latMax: 2.9,
+    lonMin: 101.65,
+    lonMax: 102.25,
+  },
+  {
+    prefix: '/seremban-b',
+    name: 'Seremban',
+    latMin: 2.35,
+    latMax: 3.15,
+    lonMin: 101.7,
+    lonMax: 102.45,
   },
 ]
 
